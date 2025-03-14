93574c5dbae120d3942c42981996057929398a83
Nikhil-Duttaroy <nsdr2000@gmail.com>
2025-03-14 17:40:59 +0530

    Created a util function to convert bytes to Size

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function getBytesToSize(bytes: number) {
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"]
+  if (bytes === 0) return "0 Byte"
+  const i = parseInt(String(Math.floor(Math.log(bytes) / Math.log(1024))))
+  return Math.round(bytes / Math.pow(1024, i)) + " " + sizes[i]
+}
