501dabeb95905a64a83bc051a16b97c9f1f0c50b
Nikhil-Duttaroy <nsdr2000@gmail.com>
2025-03-14 12:09:39 +0530

    Added navbar component to the UI

---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = ({}) => {
+  return (
+    <nav className="fixed z-50 flex items-center justify-between w-full max-h-[10vh] px-4 py-10 backdrop-blur-md bg-background bg-opacity-30 md:px-8 lg:px-12 xl:px-16 2xl:px-24 border-1">
+        <h1 className="text-4xl font-bold">Navbar</h1>
+    </nav>
+  )
+}
+
+export default Navbar
