961e6f800978bf740e5cdc1e596d947cced76a1d
Nikhil-Duttaroy <nsdr2000@gmail.com>
2025-03-15 20:34:15 +0530

    Created a NoSSRWapeer for ffmpeg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
 import Dropzone from "@/components/dropzone";
+import NoSSRWrapper from "./NoSSRWrapper";
 
 export default function Home() {
   return (
-    <main className="w-full h-full flex flex-col items-center pt-[10vh] gap-8">
-      <div className="flex flex-col items-center space-y-4 mt-12">
-        <h1 className="text-3xl font-bold">
-          Transform Your Media Files Instantly.{" "}
-        </h1>
-        <p className="text-center text-base">
-          Experience seamless media conversion directly on your device, ensuring
-          your media never leaves your device.
-        </p>
-      </div>
-      <Dropzone />
-    </main>
+    <NoSSRWrapper>
+      <main className="w-full h-full flex flex-col items-center pt-[10vh] gap-8">
+        <div className="flex flex-col items-center space-y-4 mt-12">
+          <h1 className="text-3xl font-bold">
+            Transform Your Media Files Instantly.{" "}
+          </h1>
+          <p className="text-center text-base">
+            Experience seamless media conversion directly on your device,
+            ensuring your media never leaves your device.
+          </p>
+        </div>
+        <Dropzone />
+      </main>
+    </NoSSRWrapper>
   );
 }
