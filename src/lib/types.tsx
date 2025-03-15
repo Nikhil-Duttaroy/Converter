778b61f5442b978258b9086e853c6355a7e4ad08
Nikhil-Duttaroy <nsdr2000@gmail.com>
2025-03-15 20:34:53 +0530

    Updated types for ExtendedFile

--- src/lib/types.tsx
+++ src/lib/types.tsx
@@ -1,11 +1,12 @@
 export type ExtendedFile = File & {
-    path?: string; // Add these properties
-    relativePath?: string;
-    from?: string;
-    to?: string;
-    isConverted?: boolean;
-    isConverting?: boolean;
-    isErrored?: boolean;
-    url?: string;
-    output?: unknown;
-  };
+  path?: string; // Add these properties
+  relativePath?: string;
+  from?: string;
+  to?: string;
+  isConverted?: boolean;
+  isConverting?: boolean;
+  isErrored?: boolean;
+  url?: string;
+  output?: unknown;
+  fileData?: File;
+};
